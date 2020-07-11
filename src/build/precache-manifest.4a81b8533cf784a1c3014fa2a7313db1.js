4d549525cb4c4ebc9bda71366f56faee229e15c8
kinkums <32684400+kinkums@users.noreply.github.com>
2020-07-11 15:06:43 +0100

    First Commit

---
+++ src/build/precache-manifest.4a81b8533cf784a1c3014fa2a7313db1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec94e9ca2ef295b916333c9593b9722a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2a000ccd6cd53833222",
+    "url": "/static/css/main.7b5f7dc8.chunk.css"
+  },
+  {
+    "revision": "5e259a953f3d2e97962e",
+    "url": "/static/js/2.21464644.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.21464644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2a000ccd6cd53833222",
+    "url": "/static/js/main.f7a2590d.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
